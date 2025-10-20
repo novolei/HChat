7397b8fd0cb3d50f18e54242f3ca03433b8b443c
novolei <aresray@126.com>
2025-10-20 22:14:25 +0800

    Initial Commit

---
+++ HChat/HChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HChatApp.swift
+//  HChat
+//
+//  Created by Ryan Liu on 2025/10/20.
+//
+
+import SwiftUI
+
+@main
+struct HChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
